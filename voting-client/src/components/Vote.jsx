87749c867e3907394f0f4ed6e406e0dfd755ecf7
geowarin <geowarin@gmail.com>
2015-11-18 07:35:08 +0100

    Add router and a results page

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
   },
